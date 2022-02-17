096b2a2e3f1cc5da057215e480d0323c96607239
Mike <MZbrozek@cnm.edu>
2022-02-16 21:18:57 -0700

    Completed #JS30 Challenge day 11 - added functionality to custom video player

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -61,3 +61,5 @@ progress.addEventListener('click', scrub)
 progress.addEventListener('mousemove', (e) => mousedown && scrub(e))
 progress.addEventListener('mousedown', () => mousedown = true)
 progress.addEventListener('mouseup', () => mousedown = true)
+
+//need to check on addEventListener to progress
