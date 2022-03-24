827a0ae1a14dd6e100a5a4a94a7335b5c5b3869a
Mike Zbrozek <mzbrozek@cnm.edu>
2022-03-24 16:38:57 -0600

    Started the webcam fun challenge, video running and display to canvas

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -3,3 +3,36 @@ const canvas = document.querySelector('.photo');
 const ctx = canvas.getContext('2d');
 const strip = document.querySelector('.strip');
 const snap = document.querySelector('.snap');
+
+function getVideo() {
+    navigator.mediaDevices.getUserMedia({video:true, audio: false})
+        .then(stream => {
+            console.log(stream)
+            video.srcObject = stream
+            video.play()
+        })
+        .catch(err => {
+            console.error(`Oh No!!`, err)
+        })
+}
+
+function paintToCanvas() {
+    const width = video.videoWidth
+    const height = video.videoHeight
+    canvas.width = width
+    canvas.height = height
+
+    setInterval(() => {
+        ctx.drawImage(video, 0, 0, width, height)
+
+
+    let pixels = ctx.getImageData(0,0, width, height)
+
+    pixels = rgbSplit(pixels)
+
+    ctx.putImage.Data(pixels, 0, 0)
+    }, 16)
+}
+
+
+getVideo()
